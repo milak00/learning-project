507c2a476d4f780c2dfe12209e4a8038cdf989aa
Chernetskaya Mila <chernetskaya-l@ugraweb.ru>
2023-11-03 18:36:52 +0500

    1.4 первый квартал (аккордеон с раскрывающимися пунктами меню)

---
+++ blocks/header/dropdown/headerDropdown.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+    const menuItem = document.querySelector()
+    const dropdown_menu = document.querySelector(".header__dropdown");
+    const dropdown_menu_item = document.querySelector(".header__dropdown-item");
+
+    function leftMenu() {
+        if (dropdown_menu.classList.contains("active")) {
+
+        }
+    }
+});
