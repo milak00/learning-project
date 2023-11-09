241addc6c24708d67c563f5fadbc2d1312c11534
Chernetskaya Mila <chernetskaya-l@ugraweb.ru>
2023-11-09 18:41:12 +0500

    1.4 первый квартал (аккордеон пунктов меню)

--- blocks/header/dropdown/headerDropdown.js
+++ blocks/header/dropdown/headerDropdown.js
@@ -1,11 +1,27 @@
 window.addEventListener('load', () => {
-    const menuItem = document.querySelector()
-    const dropdown_menu = document.querySelector(".header__dropdown");
-    const dropdown_menu_item = document.querySelector(".header__dropdown-item");
+    const menuItem = document.querySelector(".header__menu-item");
+    const dropdownMenu = document.querySelector(".header__menu-mobile .header__dropdown");
+    const dropdownMenuItem = document.querySelector(".header__dropdown-item");
+    const arrow = document.querySelector(".header__menu-mobile .header__menu-item::after");
+    const accordion = menuItem.contains(dropdownMenu);
+    // console.log(accordion);
 
-    function leftMenu() {
-        if (dropdown_menu.classList.contains("active")) {
 
-        }
+    for (let i = 0; i < dropdownMenu.length; i +=1) {
+        console.log(dropdownMenu[i]);
     }
+    // if (accordion === false) {
+    //     arrow.style['display'] = 'block';
+    // }
+    //
+    // function leftMenu() {
+    //     if (menuItem.classList.contains("active")) {
+    //         alert(123);
+    //     } else {
+    //         menuItem.classList.add("active");
+    //         dropdownMenu.classList.add("active");
+    //     }
+    // }
+    //
+    // menuItem.addEventListener("click", leftMenu);
 });
